0c995472f42f8847d0d547d711843da49c6e2a18
DerekF <derekforgione@gmail.com>
2016-07-08 07:13:17 -0600

    Added glm to project

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include<iostream>
+#include "glm/glm.hpp"
+#include "glm/vec3.hpp"
 
 using namespace std;
+using namespace glm;
 
 int main() {
 
 	cout << "This is a test." << endl;
+
+	vec3 a = vec3(1.0f, 2.0f, 3.0f);
+	a = glm::normalize(a);
+
+	cout << "The vector length is " << glm::length(a) << endl;
 
 	while (true);//loop forever
 	return 0;
